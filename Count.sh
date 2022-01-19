9ec7f6cfa34510c5cadcdc134dce73b45801af15
mwannier30 <93644215+mwannier30@users.noreply.github.com>
2022-01-19 17:59:33 +0100

    add description

--- Count.sh
+++ Count.sh
@@ -10,8 +10,8 @@
 #SBATCH --error=/home/mwannier/error_%j.e
 
 
-module add UHTS/Analysis/subread/2.0.1;
+module add UHTS/Analysis/subread/2.0.1; ##Add subread module v2.0.1 with featureCounts in it
 
-featureCounts -a ../Mapping/reference_genome/Homo_sapiens.GRCh38.104.gtf -o ./read_count ../Mapping/outMap/*_sorted.bam
+featureCounts -a ../Mapping/reference_genome/Homo_sapiens.GRCh38.104.gtf -o ./read_count ../Mapping/outMap/*_sorted.bam ## launch featureCounts on the files in outMap(bam files sorted on last step) with the annotation file (gtf) as argument and print the output in read_count file
 
-cut -f 1,7-12 ./read_count | tail -n +2 > ./cut_read_count
+cut -f 1,7-12 ./read_count | tail -n +2 > ./cut_read_count ## cut the resulting file to keep only the column with the gene_ID and the counts per sample and remove the first row (empty line)
